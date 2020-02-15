fc8b48b324596b3c1c815dcbb506f39ee577f098
sergiopoliveira <sergiopoliveira@outlook.com>
2020-02-15 14:21:36 +0000

    Greeting Service

--- src/main/java/org/acme/quickstart/GreetingResource.java
+++ src/main/java/org/acme/quickstart/GreetingResource.java
@@ -1,12 +1,26 @@
 package org.acme.quickstart;
 
+import org.acme.quickstart.service.GreetingService;
+import org.jboss.resteasy.annotations.jaxrs.PathParam;
+
+import javax.inject.Inject;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 
-@Path("/C:/Program Files/Git/hello")
+@Path("/hello")
 public class GreetingResource {
+
+    @Inject
+    GreetingService service;
+
+    @GET
+    @Produces(MediaType.TEXT_PLAIN)
+    @Path("/greeting/{name}")
+    public String greeting(@PathParam String name) {
+        return service.greeting(name);
+    }
 
     @GET
     @Produces(MediaType.TEXT_PLAIN)
